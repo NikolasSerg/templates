743b4fb0bba27a5e844e7fa9513a0bce13cf801b
NikoasSerg <nikolasalon@gmail.com>
2023-06-18 18:03:23 +0300

    auth methods were added

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react'
-import { auth } from "../config/firebase.config"
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth, googleProvider } from "../config/firebase.config"
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+
 
 export const Auth = () => {
     
     const [email, setEmail] = useState('');
     const [passport, setPassport] = useState('');
 
-    const signIn = () => {
-        createUserWithEmailAndPassword(auth, email, passport)
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, passport)
+        } catch (error) {
+            console.log(error, ' - error in signIn method');
+        }
     }
+
+    const googleSignIn = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider)
+        } catch (error) {
+            console.log(error, ' - error in googleSignIn method');
+        }
+    }
+
+    const logOut = async () => {
+        try {
+            await signOut(auth)
+        } catch (error) {
+            console.log(error, ' - error in logOut method');
+        }
+    }
+
+    console.log(auth?.currentUser?.email);
 
     return(
         <div>
@@ -18,6 +41,12 @@ export const Auth = () => {
             <input type="password" name="password" placeholder="Password" onInput={(e) => {setPassport(e.target.value)}} />
             <br />
             <button onClick={signIn}>SignIn</button>
+            <br />
+            <br />
+            <button onClick={googleSignIn}>Google signIN</button>
+            <br />
+            <br />
+            <button onClick={logOut}>logOut</button>
         </div>
     )
-}
+} 
--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDPhfitIY_yFZ-iTfWUavBw2KbzlIxko7Y",
@@ -12,3 +12,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
